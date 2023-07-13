c1290a829d1f20539715725d2f008a4550736ead
michaelluutrong <michaelluutrong@gmail.com>
2023-07-13 08:10:25 -0700

    commented out some code for debugging

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -1,12 +1,12 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms.validators import DataRequired, Length, URL
-from wtforms import SubmitField, StringField, DateField
-from ..api.AWS_helpers import ALLOWED_EXTENSIONS
+# from flask_wtf import FlaskForm
+# from flask_wtf.file import FileField, FileAllowed, FileRequired
+# from wtforms.validators import DataRequired, Length, URL
+# from wtforms import SubmitField, StringField, DateField
+# from ..api.AWS_helpers import ALLOWED_EXTENSIONS
 
-class ImageForm(FlaskForm):
-    image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
-    title = StringField()
-    description = StringField()
-    taken_at = DateField()
-    submit = SubmitField("Submit")
+# class ImageForm(FlaskForm):
+#     image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+#     title = StringField()
+#     description = StringField()
+#     taken_at = DateField()
+#     submit = SubmitField("Submit")
