b3a54a419f9551b80931651cc1436d4f56114a81
michaelluutrong <michaelluutrong@gmail.com>
2023-07-12 22:52:09 -0700

    redid migrations

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired, Length, URL
-from wtforms import SubmitField
-from app.routes.aws_helpers import ALLOWED_EXTENSIONS
+from wtforms import SubmitField, StringField, DateField
+from ..routes.AWS_helpers import ALLOWED_EXTENSIONS
 
 class ImageForm(FlaskForm):
     image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
-    title = TextField()
-    description = TextField()
+    title = StringField()
+    description = StringField()
     taken_at = DateField()
     submit = SubmitField("Submit")
