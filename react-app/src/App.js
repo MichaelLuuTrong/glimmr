c62bb6bf3f2a1252f3dce4fa7b9b312c8466815d
michaelluutrong <michaelluutrong@gmail.com>
2023-07-16 13:46:55 -0700

    header component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { authenticate } from "./store/session";
-import Navigation from "./components/Navigation";
+import LoginFormPage from "./components/LoginFormPage"
+import SignupFormPage from "./components/SignupFormPage";
+import Header from "./components/Header"
 
 function App() {
   const dispatch = useDispatch();
@@ -20,33 +22,42 @@ function App() {
           </Route>
           <Route path="/sign-up">
             {/* Sign-Up Page */}
+            <SignupFormPage />
           </Route>
           <Route path="/log-in">
             {/* Log-In Page */}
+            <LoginFormPage />
           </Route>
           <Route path="/explore">
             {/* Explore Page */}
+            <Header />
           </Route>
           <Route path='/photos/upload'>
             {/* Upload Photos Page */}
           </Route>
           <Route path='/photos/:photoId'>
             {/* Photo Page */}
+            <Header />
           </Route>
           <Route path='/photos/:userId/:albumsId'>
             {/* Album Page */}
+            <Header />
           </Route>
           <Route path='/people/:userId/'>
             {/* User - About Page */}
+            <Header />
           </Route>
           <Route path='/photos/:userId/photostream'>
             {/* User - Photostream Page */}
+            <Header />
           </Route>
           <Route path='/photos/:userId/albums'>
             {/* User - Albums Page */}
+            <Header />
           </Route>
           <Route path='/photos/:userId/favorites'>
             {/* User - Favorites Page */}
+            <Header />
           </Route>
         </Switch>
       )}
---
+++ react-app/src/components/Header/ProfileButton.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import { useHistory } from 'react-router-dom'
+import "./Header.css"
+
+function ProfileButton() {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const history = useHistory()
+    const ulRef = useRef();
+    const sessionUser = useSelector(state => state.session.user)
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true)
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            // Check if ulRef.current exists before accessing the 'contains' method
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener("click", closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu)
+    }, [showMenu]);
+
+    const closeMenu = () => setShowMenu(false);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+        closeMenu();
+        history.push("/explore");
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+    return (
+        <>
+            <img src={sessionUser.profile_photo} onClick={openMenu} className="headerProfilePicture changeCursor" alt="profile" />
+            <div className={ulClassName} ref={ulRef}>
+                <div className="profileMenu">
+                    <div className="hiUser">Hi, {sessionUser.first_name} {sessionUser.last_name}!</div>
+                    <div className='headerDividerDiv'></div>
+                    <div className="logOut changeCursor" onClick={logout}>Log Out</div>
+
+                </div>
+            </div>
+        </>
+    )
+
+
+}
+
+export default ProfileButton
