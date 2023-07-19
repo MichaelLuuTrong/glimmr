ee1357551cea9a07455b9d3f37baa42bc89a1f9a
michaelluutrong <michaelluutrong@gmail.com>
2023-07-19 10:48:23 -0700

    photopage and modals completed

--- react-app/src/components/PhotoPage/index.js
+++ react-app/src/components/PhotoPage/index.js
@@ -2,14 +2,17 @@ import { useParams, NavLink, useHistory } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from "react"
 import { getPhotoThunk } from "../../store/photo"
-import { deleteCommentThunk, getPhotoCommentsThunk, postCommentThunk } from "../../store/comment"
+import { getPhotoCommentsThunk, postCommentThunk } from "../../store/comment"
 import { getAllUsersThunk } from "../../store/user"
 import "./PhotoPage.css"
+import OpenModalButton from "../OpenModalButton"
+import DeleteCommentModal from "../DeleteCommentModal"
+import UpdateCommentModal from "../UpdateCommentModal"
 
 function timeSinceCommentDate(commentDate) {
     const now = new Date();
     const past = new Date(commentDate);
-    const timeDifference = now - past;
+    const timeDifference = now - past - 86400000;
     const millisecondsPerDay = 1000 * 60 * 60 * 24;
     const millisecondsPerMonth = millisecondsPerDay * 30;
     const millisecondsPerYear = millisecondsPerDay * 365;
@@ -77,6 +80,8 @@ function PhotoPage() {
             text: commentText
         }
         dispatch(postCommentThunk(photoId, user.id, commentObject))
+        dispatch(getPhotoCommentsThunk(photoId))
+        dispatch(getPhotoThunk(photoId))
         setCommentText("")
     }
 
@@ -85,10 +90,6 @@ function PhotoPage() {
         dispatch(getPhotoCommentsThunk(photoId))
         dispatch(getAllUsersThunk())
     }, [dispatch, photoId])
-
-    const handleDeleteComment = (commentId) => {
-        dispatch(deleteCommentThunk(commentId))
-    };
 
     return (
         <div className="wholePhotoDiv">
@@ -146,12 +147,6 @@ function PhotoPage() {
                     <div className="allCommentsDiv">
                         {(Object.values(commentsObj)).map(comment => (
                             <div className="wholeCommentDiv" key={comment.id}>
-                                {comment.user_id === user.id &&
-                                    <div className="deleteCommentButtonIcon"
-                                        onClick={() => handleDeleteComment(comment.id)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z" /></svg>
-                                    </div>
-                                }
                                 <img className="commentUserProfilePhoto changeCursor" src={comment.user.profile_photo} onClick={() => history.push(`/photos/${comment.user.id}/photostream`)} />
                                 <div className='commentUserNameandTimeAndComment'>
                                     <div className="commentUserNameAndTime">
@@ -162,6 +157,32 @@ function PhotoPage() {
                                     </div>
                                     <div className="commentTextDiv">{comment.text}</div>
                                 </div>
+                                {comment.user_id === user.id && (
+                                    <div className="editAndDeleteButtonsWrapper">
+                                        <div className="editAndDeleteButtons">
+                                            <OpenModalButton
+                                                buttonImage="Delete"
+                                                modalComponent={
+                                                    <DeleteCommentModal
+                                                        comment_id={comment.id}
+                                                        photo_id={photoId}
+
+                                                    />
+                                                }
+                                            />
+                                            <OpenModalButton
+                                                buttonImage="Edit"
+                                                modalComponent={
+                                                    <UpdateCommentModal
+                                                        comment_id={comment.id}
+                                                        comment_text={comment.text}
+                                                        photo_id={photoId}
+                                                    />
+                                                }
+                                            />
+                                        </div>
+                                    </div>
+                                )}
                             </div>
                         ))}
                         {user && <div className="newCommentDiv">
---
+++ react-app/src/components/UpdateCommentModal/index.js
@@ -0,0 +1,54 @@
+import { useDispatch } from "react-redux"
+import { useModal } from "../../context/Modal"
+import { patchCommentThunk, getCommentThunk, getPhotoCommentsThunk } from "../../store/comment"
+import { useState, useEffect } from "react"
+import "./UpdateCommentModal.css"
+
+const UpdateCommentModal = ({ comment_id, comment_text, photo_id }) => {
+    const dispatch = useDispatch();
+    const { closeModal } = useModal();
+    const [comment, setComment] = useState(comment_text)
+
+    const handleUpdateComment = async (e) => {
+        e.preventDefault()
+        await dispatch(patchCommentThunk(comment, comment_id,))
+        await dispatch(getPhotoCommentsThunk(photo_id))
+        closeModal()
+    }
+
+    useEffect(() => {
+        dispatch(getCommentThunk(comment_id));
+    }, [dispatch, comment_id]);
+
+    return (
+        <div className="updateCommentDiv">
+            <div className="updateCommentText">Update Comment</div>
+            <form onSubmit={handleUpdateComment}>
+                <textarea
+                    className="updateCommentTextInput"
+                    type="text"
+                    placeholder="Edit your comment"
+                    value={comment}
+                    maxLength={500}
+                    onChange={(e) => setComment(e.target.value)}
+                    required
+                >
+                </textarea>
+                <div className="bothButtonsDiv">
+                    <div className="cancelUpdateButton">
+                        <button onClick={closeModal}>
+                            Cancel
+                        </button>
+                    </div>
+                    <div className="confirmUpdateButton">
+                        <button type='submit'>
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default UpdateCommentModal
